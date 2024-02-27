7dce3d750eb7c0cf2b91498018d9f65806a53ab8
Thiago Fernando <thiago.fernando@msn.com>
2024-02-27 16:04:33 -0400

    inital

---
+++ oo-sabor-express/oo-sabor-express/modelos/restaurante.py
@@ -0,0 +1,31 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'Nome: {self.nome} | Categoria: {self.categoria}'
+
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            
+            
+    @property 
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Desativado'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+
+restaurante_pizza = Restaurante('Pizzaria BoLoLo LaLa', 'Lanches')
+restaurante_pizza.alternar_estado()
+
+Restaurante.listar_restaurantes()
