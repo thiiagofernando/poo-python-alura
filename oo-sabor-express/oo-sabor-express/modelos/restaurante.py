f1316c7491cdff1921826bb0c6c5e9d293f29e4f
Thiago Fernando <thiago.fernando@msn.com>
2024-02-29 14:32:57 -0300

    exibindo a media na tela

--- oo-sabor-express/oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/oo-sabor-express/modelos/restaurante.py
@@ -14,9 +14,9 @@ class Restaurante:
 
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliacao'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
             
             
     @property 
